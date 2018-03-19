5a9357ca2654abc331ee6a8e73fb9355062af7f5
lorderebos <37519481+lorderebos@users.noreply.github.com>
2018-03-19 23:31:49 +0800

    Update TaxiPrototype.py

--- TaxiPrototype.py
+++ TaxiPrototype.py
@@ -1,18 +1,18 @@
 import pygame
-pygame.init() #initialize pygame, always
+pygame.init() #initializes pygame
 
 win = pygame.display.set_mode((500, 500))
-# coordinates top left
+# coordinates are 0,0 on top left with pygame
 pygame.display.set_caption("First Game")
 
 screenwidth = 500
 
-red = (255, 0, 0)
+red = (255, 0, 0) #pygame uses RGB code for color
 yellow = (255, 255, 0, 1) 
 x = 50 #x starting chord for player
 y = 50 #y starting chord for player
-width = 40
-height = 40
+width = 40 # we'll use this for our player for now
+height = 40 # ^
 velocity = 5 #velocity
 z = 450 #x chord for target
 q = 450 #y chord for target
@@ -23,27 +23,27 @@ while run:
     pygame.time.delay(100) #100 milliseconds
     
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: #if you hit the red X to close the porgram, it will stop the loop
             run = False
     
     keys = pygame.key.get_pressed()
-    
+    #border detection
     if keys[pygame.K_LEFT] and x > velocity:
-        x -= vel
+        x -= velocity
     if keys[pygame.K_RIGHT] and x < screenwidth - width:
-        x += vel
+        x += velocity
     if keys[pygame.K_UP] and y > velocity:
-        y -= vel
+        y -= velocity
     if keys[pygame.K_DOWN] and y < screenwidth - height:
-        y += vel
+        y += velocity
 
     win.fill((0, 0, 0))
     player = pygame.draw.rect(win, (red), (x, y, width, height))
-    player
+    player #drawing our player proto
     target = pygame.draw.rect(win, (yellow), (z, q, width, height))
-    target
-    if player == target:
-        win.fill((yellow))
+    target #drawing our target proto, the place we want to bring our player to
+    if player == target: #if they are the same position (...) will happen
+        win.fill((yellow)) #placeholder, can add some relevant function later, for now the screen will turn yellow
     pygame.display.update()
     
 pygame.draw.rect
