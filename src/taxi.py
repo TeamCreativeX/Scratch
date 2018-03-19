ae9aa3fc98b2ac3b762932d30cf5443b5b4a6a66
Eloi <e.cousinet@gmail.com>
2018-03-19 19:11:57 +0000

    Fix(Use PEP8 naming convention, filename should be lower case, constant should be uppercase)

--- src/taxi.py
+++ src/taxi.py
@@ -1,14 +1,25 @@
 import pygame
+
+# WINDOW SIZE
+WIDTH = 800
+HEIGHT = 600
+
+# COLORS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# STRINGS
+NAME = "Scratch"
+
+# INITIALIZE PYGAME
 pygame.init() #initializes pygame
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(NAME)
 
-win = pygame.display.set_mode((500, 500))
-# coordinates are 0,0 on top left with pygame
-pygame.display.set_caption("First Game")
-
-screenwidth = 500
-
-red = (255, 0, 0) #pygame uses RGB code for color
-yellow = (255, 255, 0, 1) 
 x = 50 #x starting chord for player
 y = 50 #y starting chord for player
 width = 40 # we'll use this for our player for now
@@ -30,22 +41,20 @@ while run:
     #border detection
     if keys[pygame.K_LEFT] and x > velocity:
         x -= velocity
-    if keys[pygame.K_RIGHT] and x < screenwidth - width:
+    if keys[pygame.K_RIGHT] and x < WIDTH - width:
         x += velocity
     if keys[pygame.K_UP] and y > velocity:
         y -= velocity
-    if keys[pygame.K_DOWN] and y < screenwidth - height:
+    if keys[pygame.K_DOWN] and y < HEIGHT - height:
         y += velocity
 
     win.fill((0, 0, 0))
-    player = pygame.draw.rect(win, (red), (x, y, width, height))
+    player = pygame.draw.rect(win, (RED), (x, y, width, height))
     player #drawing our player proto
-    target = pygame.draw.rect(win, (yellow), (z, q, width, height))
+    target = pygame.draw.rect(win, (YELLOW), (z, q, width, height))
     target #drawing our target proto, the place we want to bring our player to
     if player == target: #if they are the same position (...) will happen
-        win.fill((yellow)) #placeholder, can add some relevant function later, for now the screen will turn yellow
+        win.fill((YELLOW)) #placeholder, can add some relevant function later, for now the screen will turn yellow
     pygame.display.update()
 
-pygame.quit()
-
-    
+pygame.quit() 
