b9b2ed32bc9f4c155cd17a68067582c86796a3f7
MrDuck6 <mrduckandduck6@gmail.com>
2018-03-19 21:54:24 +0000

    forgot to save file now commited properly

--- src/taxi.py
+++ src/taxi.py
@@ -1,8 +1,8 @@
 import pygame
 
 # WINDOW SIZE
-WIDTH = 800
-HEIGHT = 600
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 
 # COLORS
 WHITE = (255, 255, 255)
@@ -13,12 +13,12 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
 # STRINGS
-NAME = "Scratch"
+title = "Scratch"
 
 # INITIALIZE PYGAME
 pygame.init() #initializes pygame
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption(NAME)
+game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption(title)
 
 x = 50 #x starting chord for player
 y = 50 #y starting chord for player
@@ -41,20 +41,20 @@ while run:
     #border detection
     if keys[pygame.K_LEFT] and x > velocity:
         x -= velocity
-    if keys[pygame.K_RIGHT] and x < WIDTH - width:
+    if keys[pygame.K_RIGHT] and x < WINDOW_WIDTH - width:
         x += velocity
     if keys[pygame.K_UP] and y > velocity:
         y -= velocity
-    if keys[pygame.K_DOWN] and y < HEIGHT - height:
+    if keys[pygame.K_DOWN] and y < WINDOW_HEIGHT - height:
         y += velocity
 
-    win.fill((0, 0, 0))
-    player = pygame.draw.rect(win, (RED), (x, y, width, height))
+    game_window.fill((0, 0, 0))
+    player = pygame.draw.rect(game_window, (RED), (x, y, width, height))
     player #drawing our player proto
-    target = pygame.draw.rect(win, (YELLOW), (z, q, width, height))
+    target = pygame.draw.rect(game_window, (YELLOW), (z, q, width, height))
     target #drawing our target proto, the place we want to bring our player to
     if player == target: #if they are the same position (...) will happen
-        win.fill((YELLOW)) #placeholder, can add some relevant function later, for now the screen will turn yellow
+        game_window.fill((YELLOW)) #placeholder, can add some relevant function later, for now the screen will turn yellow
     pygame.display.update()
 
 pygame.quit() 
