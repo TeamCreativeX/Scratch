4f1c2974cdb3cd99352c2b9197753479bc9292e0
ivymarkwell126 <ivy.markwell@utexas.edu>
2018-03-20 23:30:05 -0400

    removed unnecessary code

--- src/menu.py
+++ src/menu.py
@@ -8,6 +8,7 @@ class Menu:
     '''
 
     def __init__(self, screen):
+        ''' Initialize menu atttributes '''
         self.game_active = False
         self.screen = screen
         self.num_buttons = 4
@@ -16,16 +17,8 @@ class Menu:
 
         self.prep_buttons(self.screen)
 
-    def draw_button(self, x_cord, y_cord, button_height, message):
-        # Create buttons and append to buttons list
-        button = pygame.draw.rect(self.screen, self.button_color, (x_cord, y_cord, self.width, button_height), 0)
-        self.msg_image = self.font.render(message, True, self.text_color, self.button_color)
-        self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_rect.center = button.center
-        self.screen.blit(self.msg_image, self.msg_image_rect)
-
     def prep_buttons(self, screen):
-        # Draw buttons on the screen
+        # Call button class to create individual buttons
         screen_rect = screen.get_rect()
         screen_height = screen_rect.height
         button_height = screen_height // ((self.num_buttons * 2) + 1)
